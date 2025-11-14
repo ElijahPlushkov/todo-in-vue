f71dee139c8992b7a5c2918d3eb22e5e82aea56c
Elijah <plushabeststyle@gmail.com>
2025-11-14 20:36:16 +0300

    added vue components

---
+++ components/LogoComp.js
@@ -0,0 +1,9 @@
+const LogoComp = {
+    template: `
+    <img src="logo.jpg" alt="Logo" width="30" height="24" class="d-inline-block align-text-top">
+    `,
+    data() {
+        return {}
+    }
+}
+
---
+++ components/TitleComp.js
@@ -0,0 +1,12 @@
+const TitleComp = {
+    template: `
+    <div>
+        <h1>{{ title }}</h1>
+    </div>`,
+
+    data() {
+        return {
+            title: "ToDo List"
+        }
+    }
+}
