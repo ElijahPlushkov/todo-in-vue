14107c898c86eaea6533ca36a1bcf47860ece01f
ElijahPlushkov <plushabeststyle@gmail.com>
2025-11-18 19:31:46 +0300

    built a button to add subtasks

--- components/TodoCardComp.js
+++ components/TodoCardComp.js
@@ -1,14 +1,19 @@
 const TodoCardComp = {
     template: `
-        <div class="card my-card" style="width: 18rem;">
+        <div class="card my-card" :class="selectedTodoId === todo.id ? 'selected-todo-border' : ''"
+        style="width: 18rem;" >
             <div class="card-body" :class="todo.isDone ? 'completetodo' : 'incompletetodo'">
+            <p class="card-text">{{ selectedTodoId }}</p>
+            <p class="card-text">{{ todo.id }}</p>
+            
                 <h1>{{todo.id}}</h1>
                 <h5 class="card-title">{{ todo.name }}</h5>
                 <p class="card-text">{{ todo.description }}</p>
                 <a href="#" class="btn btn-primary" @click="$emit('change-todo-status', todo)">{{ todo.isDone ? "undo" :
                         "done" }}</a>
                         <a href="#" class="btn btn-danger" @click="$emit('delete-todo', todo.id)">delete todo</a>
+                        <a href="#" class="btn btn-success" @click="$emit('select-todo-id', todo.id)">add subtasks</a>
             </div>
         </div>`,
-    props: ['todo']
+    props: ['todo', 'selectedTodoId']
 }
